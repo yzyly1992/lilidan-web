9e0a3d3b3248c2ef4fdfcca0cf3f7fa3b06ff43b
David Yang <zyang03@risd.edu>
2024-02-15 17:01:51 -0800

    develop header with responsive navbar

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  toggleValue: boolean = false;
+  toggleMenu() {
+    this.toggleValue = !this.toggleValue;
+  }
 }
