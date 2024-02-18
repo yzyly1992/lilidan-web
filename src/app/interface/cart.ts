ed218b01b2b532b8eaf7f28feb1621aa59e5e368
David Yang <zyang03@risd.edu>
2024-02-18 10:49:51 -0800

    add observable to cart service

--- src/app/interface/cart.ts
+++ src/app/interface/cart.ts
@@ -1,9 +1,10 @@
+import { BehaviorSubject, Observable } from "rxjs";
 import { Product } from "./product";
 
 export interface ICart {
-    products: Product[];
-    getCount(): number;
-    getAmount(): number;
+    products$: Observable<Product[]>;
+    count$: Observable<number>;
+    amount$: Observable<number>;
     addProduct(product: Product): void;
     removeProduct(product: Product): void;
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,30 +1,37 @@
 import { Injectable } from "@angular/core";
 import { ICart } from "../interface/cart";
 import { Product } from "../interface/product";
+import { BehaviorSubject, Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
   })
 export class Cart implements ICart{
-    products: Product[];
+    private _products: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
+    private _count: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+    private _amount: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
-    constructor() {
-        this.products = [];
-    }
-
-    getCount(): number {
-        return this.products.length;
-    }
-
-    getAmount(): number {
-        return this.products.reduce((total, product) => total + product.discount, 0);
-    }
+    products$: Observable<Product[]> = this._products.asObservable();
+    count$: Observable<number> = this._count.asObservable();
+    amount$: Observable<number> = this._amount.asObservable();
 
     addProduct(product: Product): void {
-        this.products.push(product);
+        const updatedProducts = [...this._products.value, product];
+        this._products.next(updatedProducts);
+        this._count.next(updatedProducts.length);
+        this._amount.next(this._amount.value + product.discount);
     }
 
     removeProduct(product: Product): void {
-        this.products = this.products.filter(p => p.id !== product.id);
+        // remove only one product from the list
+        for (let i = 0; i < this._products.value.length; i++) {
+            if (this._products.value[i].id === product.id) {
+                const updatedProducts = this._products.value.splice(i, 1);
+                this._products.next(updatedProducts);
+                this._count.next(updatedProducts.length);
+                this._amount.next(this._amount.value - product.discount);
+                return;
+            }
+        }
     }
 }
