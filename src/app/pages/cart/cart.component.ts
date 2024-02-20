6f0f77c582e79505f165e4ce3015326d46274dc4
David Yang <zyang03@risd.edu>
2024-02-20 12:22:18 -0800

    add paypal payment button, fix header style

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { Product } from '../../interface/product';
 import { CartService } from '../../services/cart.service';
 import { DecimalPipe, NgFor, NgIf } from '@angular/common';
@@ -15,6 +15,7 @@ export class CartComponent implements OnInit{
   count: number = 0;
   amount: number = 0;
   cartService: CartService = inject(CartService);
+  @ViewChild('paymentRef', { static: true }) paymentRef!: ElementRef;
 
   ngOnInit(): void {
     this.cartService.products$.subscribe((products: Product[]) => {
@@ -26,6 +27,47 @@ export class CartComponent implements OnInit{
     this.cartService.amount$.subscribe((amount: number) => {
       this.amount = amount;
     });
+    window.paypal.Buttons({
+      createOrder: (data: any, actions: any) => {
+        return actions.order.create({
+          purchase_units: [
+            {
+              amount: {
+                value: this.amount,
+                currency_code: 'USD',
+                breakdown:{
+                  item_total:{
+                      currency_code: 'USD',
+                      value: this.amount,
+                  }
+                }
+              },
+              items: this.products.map((product: Product) => {
+                return {
+                  name: product.name,
+                  quantity: product.count,
+                  unit_amount: {
+                    currency_code: 'USD',
+                    value: product.discount,
+                  },
+                };
+              }),
+            },
+          ],
+        });
+      },
+      onApprove: (data: any, actions: any) => {
+        return actions.order.capture().then((details: any) => {
+          if (details.status === 'COMPLETED') {
+            console.log(details.id);
+            this.cartService.clearCart();
+          }
+        });
+      },
+      onError: (err: any) => {
+        console.log(err);
+      },
+    }).render(this.paymentRef.nativeElement);
   }
 
   removeProduct(product: Product): void {
