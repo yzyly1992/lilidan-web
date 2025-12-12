64c0896e9e583dc1f260c61fc93362ab7d9e87e0
David Yang <yang.zhiyu@northeastern.edu>
2025-12-11 18:07:42 -0800

    Disable the button, add banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { AnnouncementComponent } from './layout/announcement/announcement.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, FooterComponent, HeaderComponent, AnnouncementComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/layout/announcement/announcement.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-announcement',
+  standalone: true,
+  imports: [],
+  templateUrl: './announcement.component.html',
+  styleUrl: './announcement.component.scss'
+})
+export class AnnouncementComponent {
+
+}
